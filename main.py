7ddee338d160de69c8c26c5f965ca524a13839e8
kannbo <113285773+kannbo@users.noreply.github.com>
2024-03-19 16:06:23 +0900

    Create main.py

---
+++ main.py
@@ -0,0 +1,11 @@
+import streamlit as st,uuid,os
+import requests as req
+st.header("shorking url")
+st.write("このアプリはis.gdを用いてショートURLを生成します")
+url=st.text_input("URL")
+if st.button("作成"):
+  try:
+    _url=request.get(f"https://is.gd/create.php?format=simple&url={url}").text
+    st.write(f"生成完了!:{_url}"
+  except Exception as e:
+    st.write(f"error:{e}")

c045c10080eb567165540ba0e581d09624ebbc15
kannbo <113285773+kannbo@users.noreply.github.com>
2024-03-19 16:20:46 +0900

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,11 @@ st.write("このアプリはis.gdを用いてショートURLを生成します")
 url=st.text_input("URL")
 if st.button("作成"):
   try:
-    _url=request.get(f"https://is.gd/create.php?format=simple&url={url}").text
+    _url=req.get(f"https://is.gd/create.php?format=simple&url={url}").text
     st.write(f"生成完了!:{_url}")
   except Exception as e:
-    st.write("<style>red:{  color:red;}</style><red>生成失敗!:"+str(e)+"</red>",unsafe_allow_html=True)
+    st.write("""<style>
+    red{
+      color:red;
+      }
+    </style>"""+"<red>生成失敗!:"+str(e)+"</red>",unsafe_allow_html=True)

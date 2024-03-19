2a67a49ab96dc8ce05d44582bad30df17cec4a15
kannbo <113285773+kannbo@users.noreply.github.com>
2024-03-19 16:22:45 +0900

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,14 @@ url=st.text_input("URL")
 if st.button("作成"):
   try:
     _url=req.get(f"https://is.gd/create.php?format=simple&url={url}").text
-    st.write(f"生成完了!:{_url}")
+    if not _url=="Error: Please enter a valid URL to shorten":
+      st.write(f"生成完了!:{_url}")
+    else:
+          st.write("""<style>
+    red{
+      color:red;
+      }
+    </style>"""+"<red>生成失敗!:"+str(e)+"</red>",unsafe_allow_html=True)
   except Exception as e:
     st.write("""<style>
     red{
